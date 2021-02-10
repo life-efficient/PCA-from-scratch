492bb023c0b3cb29ee1dc76fc529f303a20d656a
catherinedoudou <xiaodouzi821@gmail.com>
2021-02-10 21:43:20 +0000

    final

--- PCA.py
+++ PCA.py
@@ -5,8 +5,9 @@ import pandas as pd     # allow us to make dataframes to store our data cleanly
 from sklearn import datasets  
 
 class PCA:
-  def __init__(self):
-    pass
+  def __init__(self, n_com):
+      self.n_com = n_com
+  
   
   def fit(self, X):
     """
@@ -17,24 +18,24 @@ class PCA:
     X /= m
     cov = X.T @ X
     eigenvalues, eigenvector = np.linalg.eig(cov)
-    print('hi')
+    print(eigenvector)
+    print(eigenvalues)
+    X = X @ eigenvector
+    return X[:,:self.n_com]
 
 if __name__ == "__main__":
-  data = pd.read_csv('../DATA/iris.csv')
-
-  label_dict = {'Setosa': 0, 'Versicolor': 1, 'Virginica': 2}  # dictionary containing label to number mapping
-  data["variety"] = data["variety"].replace(label_dict)
-
-  X = data[["sepal.length", "sepal.width", "petal.length", "petal.width"]]
-  Y = data["variety"]
+  data = datasets.load_iris()
+  X = data['data']
+  Y = data['target']
 
   print(X.shape)      # 150 rows (datapoints), 4 columns (features)
   print(Y.shape)      # 150 single dimension labels
 
   m = X.shape[0]      # 150 rows
 
-  pca = PCA()
-  pca.fit(X)
+  pca = PCA(n_com=2)
+  x_transfrom = pca.fit(X)
+  print(x_transfrom)
 
 
 
